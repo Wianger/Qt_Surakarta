902a67ac442cde7249656a9117192a2f6f30255a
Wiang <2692807124@qq.com>
2024-04-22 12:27:02 +0800

    QT

--- main.cpp
+++ main.cpp
@@ -6,9 +6,9 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    MainWindow w;
     ChooseDlg dlg;
     while(dlg.run()){
-        MainWindow w;
         w.show();
         return a.exec();
     }
